460d33f0ea72e872b7fb2886bc5671d865ae0655
J <kj5592305@gmail.com>
2025-03-25 19:30:15 +0900

    feat : Gradle 기반 프로젝트 초기 세팅 및 개발 환경 구성

---
+++ src/test/java/com/example/concertreservation/ConcertReservationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.concertreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
