9262b0651121f3db55710af0237ed060b09ec816
yrchoi <daylikezero@gmail.com>
2025-03-26 23:13:25 +0900

    refactor: 콘서트 예약 서비스 수정 -> Lock 테스트 코드 리팩토링

--- src/main/java/com/example/concertreservation/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/concertreservation/domain/reservation/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package com.example.concertreservation.domain.reservation.repository;
 import com.example.concertreservation.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Optional<Reservation> findByUserIdAndConcertId(Long userId, Long concertId);
 }
--- src/test/java/com/example/concertreservation/domain/reservation/LettuceLockReservationTest.java
+++ src/test/java/com/example/concertreservation/domain/reservation/LettuceLockReservationTest.java
@@ -2,7 +2,9 @@ package com.example.concertreservation.domain.reservation;
 
 import com.example.concertreservation.common.enums.UserRole;
 import com.example.concertreservation.domain.concert.entity.Concert;
+import com.example.concertreservation.domain.concert.entity.ConcertReservationDate;
 import com.example.concertreservation.domain.concert.repository.ConcertRepository;
+import com.example.concertreservation.domain.concert.repository.ConcertReservationDateRepository;
 import com.example.concertreservation.domain.reservation.facade.LettuceLockReservationFacade;
 import com.example.concertreservation.domain.reservation.repository.ReservationRepository;
 import com.example.concertreservation.domain.user.entity.User;
@@ -13,9 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,39 +35,62 @@ public class LettuceLockReservationTest {
     @Autowired
     private ConcertRepository concertRepository;
     @Autowired
+    private ConcertReservationDateRepository concertReservationDateRepository;
+    @Autowired
     private UserRepository userRepository;
 
-    public static int CAPACITY = 100;
+    public static final int CAPACITY = 100;
+    public static final int THREAD_COUNT = 1_000;
 
     private Concert concert;
-    private User user;
 
     @BeforeEach
     public void setUp() {
-        concert = Concert.builder().title("콘서트").capacity(CAPACITY).availableAmount(CAPACITY).build();
+        concert = Concert.builder()
+                .title("콘서트")
+                .capacity(CAPACITY)
+                .availableAmount(CAPACITY)
+                .build();
         concertRepository.saveAndFlush(concert);
-        user = new User("test", "1234", UserRole.ROLE_USER);
-        userRepository.saveAndFlush(user);
+
+        concertReservationDateRepository.saveAndFlush(
+                ConcertReservationDate.builder()
+                        .concert(concert)
+                        .startDate(LocalDateTime.of(0000, 1, 1, 0, 0))
+                        .endDate(LocalDateTime.of(9999, 12, 31, 23, 59))
+                        .build()
+        );
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            users.add(User.builder()
+                    .email("user" + i + "@gmail.com")
+                    .password("1234")
+                    .userRole(UserRole.ROLE_USER)
+                    .build());
+        }
+        userRepository.saveAllAndFlush(users);
     }
 
     @AfterEach
     public void tearDown() {
         reservationRepository.deleteAll();
+        concertReservationDateRepository.deleteAll();
         concertRepository.deleteAll();
         userRepository.deleteAll();
     }
 
     @Test
     public void 동시에_콘서트_예매_요청() throws InterruptedException {
-        int threadCount = 1_000;
         ExecutorService executorService = Executors.newFixedThreadPool(100);
-        CountDownLatch latch = new CountDownLatch(threadCount);
+        CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
 
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < threadCount; i++) {
+        AtomicInteger count = new AtomicInteger(1);
+        for (int i = 0; i < THREAD_COUNT; i++) {
             executorService.submit(() -> {
                 try {
-                    lettuceLockReservationFacade.create(concert.getId(), user.getId());
+                    lettuceLockReservationFacade.create(concert.getId(), (long) count.getAndIncrement());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 } finally {
@@ -70,9 +99,10 @@ public class LettuceLockReservationTest {
             });
         }
         latch.await();
+        executorService.shutdown();
 
         long endTime = System.currentTimeMillis();
-        System.out.println(CAPACITY + " 예약 가능, " + threadCount + "개 요청 처리 시간: " + (endTime - startTime) + "ms");
+        System.out.println(CAPACITY + " 예약 가능, " + THREAD_COUNT + "개 요청 처리 시간: " + (endTime - startTime) + "ms");
 
         Concert updatedConcert = concertRepository.findById(concert.getId()).get();
         assertEquals(0, updatedConcert.getAvailableAmount());
