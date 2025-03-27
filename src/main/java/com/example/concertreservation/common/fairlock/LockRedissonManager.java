e60d0a5b01ad3a0bd3704f659b4121af7a7b491f
J <kj5592305@gmail.com>
2025-03-27 21:28:31 +0900

    로그 레벨 적용

--- src/main/java/com/example/concertreservation/common/fairlock/LockRedissonManager.java
+++ src/main/java/com/example/concertreservation/common/fairlock/LockRedissonManager.java
@@ -3,6 +3,8 @@ package com.example.concertreservation.common.fairlock;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -10,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 @Component
 @RequiredArgsConstructor
 public class LockRedissonManager implements LockManager {
-
+    private static final Logger log = LoggerFactory.getLogger(LockRedissonManager.class);
     private final RedissonClient redissonClient;
     private static final String LOCK_KEY_PREFIX = "concert-reservation-lock:";
     private static final long WAIT_TIME = 10;
@@ -21,21 +23,21 @@ public class LockRedissonManager implements LockManager {
         String lockKey = LOCK_KEY_PREFIX + key;
         RLock lock = redissonClient.getFairLock(lockKey);
 
-        System.out.println("락 획득 시도: " + lockKey);
+        log.info("락 획득 시도: {}", lockKey);
 
         if (lock.tryLock(WAIT_TIME, LEASE_TIME, TimeUnit.SECONDS)) {
             try {
-                System.out.println("락 획득 성공: " + lockKey);
+                log.info("락 획득 성공: {}", lockKey);
                 task.run();
             } catch (Exception e) {
-                System.out.println("람다 내부에서 예외 발생: " + e.getMessage());
+                log.error("람다 내부에서 예외 발생: {}", e.getMessage(), e);
                 e.printStackTrace();
             } finally {
                 lock.unlock();
-                System.out.println("락 해제: " + lockKey);
+                log.info("락 해제: {}", lockKey);
             }
         } else {
-            System.out.println("락 획득 실패: " + lockKey);
+            log.warn("락 획득 실패: {}", lockKey);
             throw new IllegalStateException("[LockExecutor] 락 획득 실패: " + lockKey);
         }
     }
