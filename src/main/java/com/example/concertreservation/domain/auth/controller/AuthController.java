758b1d7bcc45a499e8e77c3bdc5ad72dd309ef2c
Doritos38 <mydjkim@naver.com>
2025-03-27 14:22:25 +0900

    Feat: 로그인 관련 예외처리와 api 작업
    # Conflicts:
    #	src/main/resources/application-dev.properties
    
    # Conflicts:
    #	src/main/resources/application-dev.properties
    
    # Conflicts:
    #	src/main/resources/application-dev.properties

--- src/main/java/com/example/concertreservation/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/concertreservation/domain/auth/controller/AuthController.java
@@ -7,6 +7,8 @@ import com.example.concertreservation.domain.auth.dto.response.SignupResponse;
 import com.example.concertreservation.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +20,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/signup")
-    public SignupResponse signup(@Valid @RequestBody SignupRequest signupRequest) {
-        return authService.signup(signupRequest);
+    public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest signupRequest) {
+        authService.signup(signupRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/auth/signin")
-    public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
-        return authService.signin(signinRequest);
+    public ResponseEntity<SigninResponse> signin(@Valid @RequestBody SigninRequest signinRequest) {
+        return ResponseEntity.ok(authService.signin(signinRequest));
     }
 }
--- src/main/java/com/example/concertreservation/domain/auth/service/AuthService.java
+++ src/main/java/com/example/concertreservation/domain/auth/service/AuthService.java
@@ -1,11 +1,12 @@
 package com.example.concertreservation.domain.auth.service;
 
 import com.example.concertreservation.common.enums.UserRole;
+import com.example.concertreservation.common.exception.InvalidAuthenticationException;
+import com.example.concertreservation.common.exception.InvalidRequestException;
 import com.example.concertreservation.config.JwtUtil;
 import com.example.concertreservation.domain.auth.dto.request.SigninRequest;
 import com.example.concertreservation.domain.auth.dto.request.SignupRequest;
 import com.example.concertreservation.domain.auth.dto.response.SigninResponse;
-import com.example.concertreservation.domain.auth.dto.response.SignupResponse;
 import com.example.concertreservation.domain.user.entity.User;
 import com.example.concertreservation.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,10 +24,10 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     @Transactional
-    public SignupResponse signup(SignupRequest signupRequest) {
+    public void signup(SignupRequest signupRequest) {
 
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
-            throw new RuntimeException("이미 존재하는 이메일입니다.");
+            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
 
         String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
@@ -38,20 +39,17 @@ public class AuthService {
                 encodedPassword,
                 userRole
         );
-        User savedUser = userRepository.save(newUser);
 
-        String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(), userRole);
-
-        return new SignupResponse(bearerToken);
+        userRepository.save(newUser);
     }
 
     public SigninResponse signin(SigninRequest signinRequest) {
         User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
-                () -> new RuntimeException("가입되지 않은 유저입니다."));
+                () -> new InvalidRequestException("가입되지 않은 유저입니다."));
 
         // 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
         if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
-            throw new RuntimeException("잘못된 비밀번호입니다.");
+            throw new InvalidAuthenticationException("잘못된 비밀번호입니다.");
         }
 
         String bearerToken = jwtUtil.createToken(user.getId(), user.getEmail(), user.getUserRole());
