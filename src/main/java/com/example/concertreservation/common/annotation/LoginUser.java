f719f8d060bc055fe0a8469cc15fc119de83c9a5
ksmamks <minasithil33@gmail.com>
2025-03-27 17:03:59 +0900

    refactor : @LoginUser 삭제, SpringSecurity단일 사용

--- src/main/java/com/example/concertreservation/common/annotation/LoginUser.java
+++
@@ -1,8 +0,0 @@
-package com.example.concertreservation.common.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface LoginUser {
-}
--- src/main/java/com/example/concertreservation/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/example/concertreservation/domain/reservation/controller/ReservationController.java
@@ -1,10 +1,10 @@
 package com.example.concertreservation.domain.reservation.controller;
 
 import com.example.concertreservation.common.dto.AuthUser;
-import com.example.concertreservation.common.annotation.LoginUser;
 import com.example.concertreservation.domain.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ public class ReservationController {
 
     // 콘서트 예약 등록
     @PostMapping("/{concertId}")
-    public ResponseEntity<String> createReservation(@PathVariable Long concertId, @LoginUser AuthUser authUser) {
+    public ResponseEntity<String> createReservation(@PathVariable Long concertId, @AuthenticationPrincipal AuthUser authUser) {
         // 로그인 검증
         if (authUser == null) {
             return ResponseEntity.status(403).body("로그인이 필요합니다.");
