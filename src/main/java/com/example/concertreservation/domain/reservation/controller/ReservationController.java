daa1ccc47d29974b3de2da47b86eefc89f3213dc
J <kj5592305@gmail.com>
2025-03-27 13:08:18 +0900

    controller 코드 제거

--- src/main/java/com/example/concertreservation/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/example/concertreservation/domain/reservation/controller/ReservationController.java
@@ -1,24 +1,5 @@
 package com.example.concertreservation.domain.reservation.controller;
 
-import com.example.concertreservation.common.fairlock.LockReservationService;
-import com.example.concertreservation.domain.reservation.service.ReservationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/reservations")
-@RequiredArgsConstructor
 public class ReservationController {
 
-    private final ReservationService reservationService;
-    private final LockReservationService lockReservationService;
-
-    @PostMapping("/{concertId}")
-    public ResponseEntity<Void> reserveConcert(@PathVariable Long concertId,
-                                               @RequestParam Long userId) {
-        lockReservationService.executeWithLock(concertId, userId);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
 }
